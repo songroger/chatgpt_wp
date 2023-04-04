69d3b0e218718c5487a11907fc31d4de2a7cf88a
songroger <songroger@vip.qq.com>
2023-04-04 17:51:00 +0800

    fix:copy

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function App() {
           .map((it) => it.content.text)
           .join('\n')
         // console.log('messages', messages, r)
-        if ("clipboard" in navigator) {
+        if (window.isSecureContext && "clipboard" in navigator) {
           await navigator.clipboard.writeText(r)
             .then(() => toast.show('复制成功', "success", 2_000))
             .catch((error) => toast.fail('复制失败,'+ error.message, "loading", 2_000))
